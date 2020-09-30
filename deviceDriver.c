1f468935407a38331c124ec8d5dc63111222873f
Brian Peterson <bepetersn@gmail.com>
2020-09-30 12:39:08 -0600

    Not working R/W; buffer creation

--- deviceDriver.c
+++ deviceDriver.c
@@ -13,31 +13,77 @@ MODULE_LICENSE("GPL");
 
 int num_times_opened;
 int num_times_closed;
+void *buffer;
 
 int chardev_test_open(struct inode *dev, struct file* f) 
 {
-    // printk(KERN_ALERT "opened me"); 
-    //num_times_opened++;
+    num_times_opened++;
+    printk(KERN_ALERT "Num times opened: %d\n", num_times_opened); 
     return 0;
 }
 
 int chardev_test_close(struct inode *dev , struct file *f)
 {
-    //num_times_closed++;
-    //printk(KERN_ALERT "Num times opened: %d", num_times_closed);
+    num_times_closed++;
+    printk(KERN_ALERT "Num times closed: %d\n", num_times_closed);
     return 0;
 }
 
+ssize_t chardev_test_write(struct file *f, 
+                           const char __user *user_buf, 
+                           size_t buf_size, loff_t *pos)
+{
+    int result = copy_from_user(f, user_buf, buf_size);
+    if(result != 0) {
+        printk(KERN_INFO "Wrote %lu bytes\n", buf_size);
+        *pos += buf_size;
+    }
+    return buf_size;
+}
+
+ssize_t chardev_test_read(struct file *f, 
+                          char __user *user_buf, 
+                          size_t buf_size, 
+                          loff_t *pos)
+{
+    int bytes_read = 0;
+    int result = copy_to_user(user_buf, f, buf_size);
+    if(result != 0) {
+        printk(KERN_INFO "Read %lu bytes\n", buf_size);
+        bytes_read = buf_size;
+    }
+    *pos += buf_size;
+   
+    return bytes_read;
+}
+
+loff_t chardev_test_seek(struct file *dev, loff_t offset, int whence)
+{
+    // - Deal with whence
+    // - subtract or add seek if appropriate
+    // - or otherwise, set to offset
+    // - Deal with out-of-buffer issues
+    return 0;
+}
+
+
 int driver_init(void)
 {
-
     static struct file_operations fops = {
 	.owner   = THIS_MODULE,
 	.open    = chardev_test_open, 
-	.release = chardev_test_close, 
+	.release = chardev_test_close,
+        .write   = chardev_test_write,
+        .read    = chardev_test_read
     };
+    buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL)
+    if(buffer == NULL) {
+        printk(KERN_ALERT, "Buffer not allocated");
+        exit(1);
+    }
+
     register_chrdev(CHARDEV_TEST_MAJOR_NUMBER, "chardev_test", &fops);
-    printk(KERN_ALERT "inside %s function\n", __FUNCTION__);
+    printk(KERN_NOTICE "inside %s function\n", __FUNCTION__);
     return 0;
 }
 
@@ -47,7 +93,7 @@ void driver_exit(void)
 {
     unregister_chrdev(CHARDEV_TEST_MAJOR_NUMBER, "chardev_test");
     // kfree
-    printk(KERN_ALERT "inside %s function\n", __FUNCTION__);
+    printk(KERN_NOTICE "inside %s function\n", __FUNCTION__);
 }
 
 module_init(driver_init);
