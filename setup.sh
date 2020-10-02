bf86387d4507718985f0141fe760d2d5509ce713
Brian Peterson <bepetersn@gmail.com>
2020-10-01 20:39:22 -0600

    I like comments

--- setup.sh
+++ setup.sh
@@ -2,15 +2,23 @@
 
 sudo true
 make && \
-# Either rm or if that fails, insert
-(rmmod deviceDriver.ko || insmod deviceDriver.ko) && \
-insmod deviceDriver.ko && \
-# Now, ^ we either rm'ed successfully or
-# inserted it successfully; 
-# try inserting again to make sure it was the former;
 
-# then, make sure to mknod again if insertion 
-# occurred in second line above
+# NOTE: Hack to make this idempotent 
+# without my knowing bash very well
+
+# Either rm, or if that fails, insert the driver
+(rmmod deviceDriver.ko || insmod deviceDriver.ko) && \
+
+# Now, we hopefully either rm'ed successfully or
+# inserted successfully; try inserting 
+# again to give the best chance the driver is 
+# now inserted; this will complain but not fail
+# if it was already inserted
+insmod deviceDriver.ko && \
+
+# then, mknod to attach the driver to a file 
 mknod -m 777 /dev/chardev_test c 261 0
+
+# Show devices to let us know what happened
 cat /proc/devices | tail -n 30
 ls -l /dev/chardev_test
